2948ff407cf58927546ec0a619eefd1e2bf23c9b
arifkhan012 <arif.softron@gmail.com>
2021-08-18 18:34:05 +0600

    first-commit

---
+++ components/layout/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="container">
+      <div className="row">
+        <h2>Footer</h2>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ components/layout/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div className="container">
+      <div className="row">
+        <h2>Navbar</h2>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
