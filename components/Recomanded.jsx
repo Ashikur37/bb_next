1ef543a16ae11528b86982ee031e85ea696afbc0
arifkhan012 <arif.softron@gmail.com>
2021-09-08 18:49:29 +0600

    recomanded product

--- components/Recomanded.jsx
+++ components/Recomanded.jsx
@@ -5,13 +5,13 @@ import { i18n, withTranslation } from "../i18n";
 import styles from "../styles/Recomanded.module.scss";
 function Recomanded({ products, t }) {
   return (
-    <div className={styles.recomanded_container}>
+    <div className="">
       {/* <h6 className="header_component header_title">
         {t("RECOMMENDED FOR YOU")}
       </h6> */}
-      <div className={styles.recomaded_items}>
+      <div className="row">
         {products.map((product, index) => (
-          <div className="rc" key={index}>
+          <div className="col-6 col-xs-6 col-sm-6 col-md-6 col-lg-12 " key={index}>
             <Product product={product} style={styles} />
           </div>
         ))}
