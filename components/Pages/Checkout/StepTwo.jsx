1008abc4ceae802573d1d5076c5c43a7e8ce9625
Mominul Haque <sejan840@gmail.com>
2021-10-09 23:37:09 +0600

    cards

--- components/Pages/Checkout/StepTwo.jsx
+++ components/Pages/Checkout/StepTwo.jsx
@@ -42,7 +42,7 @@ function StepTwo(props) {
       data:{},
       headers: {
         Accept: 'application/json',
-        'Content-Type': 'application/json',
+        'Content-Type': 'application/x-www-form-urlencoded',
         Authorization: 'Bearer rLtt6JWvbUHDDhsZnfpAhpYk4dxYDQkbcPTyGaKp2TYqQgG7FGZ5Th_WD53Oq8Ebz6A53njUoo1w3pjU1D4vs_ZMqFiz_j0urb_BH9Oq9VZoKFoJEDAbRZepGcQanImyYrry7Kt6MnMdgfG5jn4HngWoRdKduNNyP4kzcp3mRv7x00ahkm9LAK7ZRieg7k1PDAnBIOG3EyVSJ5kK4WLMvYr7sCwHbHcu4A5WwelxYK0GMJy37bNAarSJDFQsJ2ZvJjvMDmfWwDVFEVe_5tOomfVNt6bOg9mexbGjMrnHBnKnZR1vQbBtQieDlQepzTZMuQrSuKn-t5XZM7V6fCW7oP-uXGX-sMOajeX65JOf6XVpk29DP6ro8WTAflCDANC193yof8-f5_EYY-3hXhJj7RBXmizDpneEQDSaSz5sFk0sV5qPcARJ9zGG73vuGFyenjPPmtDtXtpx35A-BVcOSBYVIWe9kndG3nclfefjKEuZ3m4jL9Gg1h2JBvmXSMYiZtp9MR5I6pvbvylU_PP5xJFSjVTIz7IQSjcVGO41npnwIxRXNRxFOdIUHn0tjQ-7LwvEcTXyPsHXcMD8WtgBh-wxR8aKX7WPSsT1O8d8reb2aR7K3rkV3K82K_0OgawImEpwSvp9MNKynEAJQS6ZHe_J_l77652xwPNxMRTMASk1ZsJL'
       }
     };
@@ -76,9 +76,7 @@ function StepTwo(props) {
           document.getElementById("card-element").classList.remove('d-none');
           var config = {
             countryCode: "KWT", // Here, add your Country Code you receive from InitiateSession Endpoint.
-            // response.data.Data.CountryCode ||
             sessionId: "e4359b89-c328-ec11-bae9-000d3aaca798", // Here you add the "SessionId" you receive from InitiateSession Endpoint.
-            // response.data.Data.SessionId ||
             cardViewId: "card-element",
           };
           myFatoorah.init(config);
